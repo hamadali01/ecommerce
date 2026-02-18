dd3e8f59846dff79144737106686e8e448257d9d
Hammad Raza <hamadalimalik0@gmail.com>
2026-02-19 03:25:47 +0500

    design for create store modal

--- ecommerce-admin/components/modals/store-modal.tsx
+++ ecommerce-admin/components/modals/store-modal.tsx
@@ -1,13 +1,77 @@
 "use client";
 
-import { Modal } from "@/components/ui/modal";
+import * as z from "zod";
+import { Controller, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
 import { useStoreModal } from "@/hooks/use-store-modal";
+import { Modal } from "@/components/ui/modal";
+import {
+    Field,
+    FieldDescription,
+    FieldError,
+    FieldGroup,
+    FieldLabel,
+    FieldSet,
+} from "@/components/ui/field"
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button";
+
+const formSchema = z.object({
+    name: z.string().min(1),
+});
 
 export const StoreModal = () => {
-    const storeModal = useStoreModal(); 
+    const storeModal = useStoreModal();
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            name: "",
+        }
+    })
+
+    const onSubmit = async (values: z.infer<typeof formSchema>) => {
+        console.log(values);
+    };
     return (
         <Modal title="Create Store" description="Add a new store to manage your products and orders" isOpen={storeModal.isOpen} onClose={storeModal.onClose}>
-            Future Create Store Form 
+            <div>
+                <div className="space-y-4 py-2 pb-4">
+                    <form id="form-rhf-textarea" onSubmit={form.handleSubmit(onSubmit)}>
+                        <FieldGroup>
+                            <Controller
+                                name="name"
+                                control={form.control}
+                                render={({ field, fieldState }) => (
+                                    <Field data-invalid={fieldState.invalid}>
+                                        <FieldLabel htmlFor="name">
+                                            Name
+                                        </FieldLabel>
+                                        <Input
+                                            {...field}
+                                            id="name"
+                                            aria-invalid={fieldState.invalid}
+                                            placeholder="E-Commerce"
+                                            autoComplete="name"
+                                        />
+                                        {fieldState.invalid && (
+                                            <FieldError errors={[fieldState.error]} />
+                                        )}
+                                    </Field>
+                                )}
+                            />
+                        </FieldGroup>
+                        <div className="pt-6 space-x-2 flex items-center justify-end w-full">
+                            <Button
+                                variant="outline"
+                                onClick={storeModal.onClose}
+                            >Cancel</Button>
+                            <Button type="submit">Continue</Button>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </Modal>
     )
 }
