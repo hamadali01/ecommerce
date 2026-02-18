50e8ee65bd18f7c8facbcfafcef12f8334e6d406
Hammad Raza <hamadalimalik0@gmail.com>
2026-02-19 02:22:36 +0500

    added modal to create new store

---
+++ ecommerce-admin/components/modals/store-modal.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { Modal } from "@/components/ui/modal";
+import { useStoreModal } from "@/hooks/use-store-modal";
+
+export const StoreModal = () => {
+    const storeModal = useStoreModal(); 
+    return (
+        <Modal title="Create Store" description="Add a new store to manage your products and orders" isOpen={storeModal.isOpen} onClose={storeModal.onClose}>
+            Future Create Store Form 
+        </Modal>
+    )
+}
